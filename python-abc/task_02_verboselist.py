ecd8baf09dc932e59408c5c2adced3780f0ba5d1
omar-hail <donz.h.h@gmail.com>
2025-12-20 05:23:53 +0000

    Custom list subclass that logs when items are added or removed.

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
-'''
-Clase VerboseList que extiende list y notifica las operaciones de adición y eliminación.
-'''
+
+
 class VerboseList(list):
-    '''
-    Extiende la clase list de Python para imprimir notificaciones
-    cada vez que se añade o elimina un elemento.
-    '''
     def append(self, item):
+        print("Added {} to the list".format(item))
         super().append(item)
-        print(f'Added [{item}] to the list.')
-    def extend(self, iterable):
-        count = len(iterable) 
-        super().extend(iterable)
-        print(f'Extended the list with [{count}] items.')
+
+    def extend(self, items):
+        print("Extended the list with {} items".format((len(items))))
+        super().extend(items)
 
     def remove(self, item):
-        print(f'Removed [{item}] from the list.')
+        print("Removed {} from the list".format(item))
         super().remove(item)
 
-    def pop(self, index = -1):
-        popped_item = self[index] 
-        print(f'Popped [{popped_item}] from the list.')
-        super().pop(index)
-
-if __name__ == "__main__":
-    lista = VerboseList([1,2,3,4])
-    lista.append(5)
-    lista_Dos = ["e","m","a"]
-    lista.extend(lista_Dos)
-    lista.remove(1)
-    lista.pop(0)
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print("Popped {} from the list".format(item))
+        return item
