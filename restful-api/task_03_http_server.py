e3f5225f9ea65baa1480f7d6ad1bddfb906e4f9f
omar-hail <donz.h.h@gmail.com>
2026-01-03 15:44:53 +0000

    Add simple HTTP API using Python http.server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class My_server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+port = 8000
+host = ""
+server_address = (host, port)
+server = HTTPServer(server_address, My_server)
+server.serve_forever()
