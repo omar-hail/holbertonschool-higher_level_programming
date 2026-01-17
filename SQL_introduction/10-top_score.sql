b6c9407e07680304b51cb0ea83f8e57e21dc6795
omar-hail <donz.h.h@gmail.com>
2026-01-17 11:59:48 +0000

    List second_table records sorted by highest score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records from second_table showing score first and name second
+-- Order the results by score in descending order (highest score first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
