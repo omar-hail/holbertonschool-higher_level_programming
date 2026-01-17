bae0334f486d2197d9bed4fc082ec2c4326a199a
omar-hail <donz.h.h@gmail.com>
2026-01-17 14:04:34 +0000

    List states starting with 'N'

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""List all states with a name starting with uppercase N from the database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states "
+        "WHERE BINARY name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
