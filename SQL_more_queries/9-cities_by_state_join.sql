8d6d1090bc482a260d3099dfcd84c5343c25d947
omar-hail <donz.h.h@gmail.com>
2026-01-17 13:06:07 +0000

    List all cities with their states

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- This script lists all cities with their corresponding state
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM cities
+JOIN states
+    ON cities.state_id = states.id
+ORDER BY cities.id ASC;
